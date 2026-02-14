4b728816339a42d5f97c28d3b067817adac98080
Sir O <sireoputa@gmail.com>
2026-02-14 22:21:47 +0000

    outline

---
+++ drills/drills_1/target/debug/deps/drills_1-0a2aeb6b6c211c87.d
@@ -0,0 +1,5 @@
+/workspaces/Rust_primer/drills/drills_1/target/debug/deps/drills_1-0a2aeb6b6c211c87.d: src/main.rs
+
+/workspaces/Rust_primer/drills/drills_1/target/debug/deps/drills_1-0a2aeb6b6c211c87: src/main.rs
+
+src/main.rs:
---
+++ drills/drills_1/target/debug/drills_1.d
@@ -0,0 +1,1 @@
+/workspaces/Rust_primer/drills/drills_1/target/debug/drills_1: /workspaces/Rust_primer/drills/drills_1/src/main.rs
