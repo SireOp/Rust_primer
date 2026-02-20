0b9d44584618bb185e0736be355b489125a04821
Sir O <sireoputa@gmail.com>
2026-02-20 12:01:47 +0000

    Update drills and memory sizes

---
+++ week1_minivec/target/debug/deps/week1_minivec-37c19f461288a2be.d
@@ -0,0 +1,5 @@
+/workspaces/Rust_primer/week1_minivec/target/debug/deps/week1_minivec-37c19f461288a2be.d: src/main.rs
+
+/workspaces/Rust_primer/week1_minivec/target/debug/deps/libweek1_minivec-37c19f461288a2be.rmeta: src/main.rs
+
+src/main.rs:
---
+++ week1_minivec/target/debug/deps/week1_minivec-92b9146ff3acad09.d
@@ -0,0 +1,5 @@
+/workspaces/Rust_primer/week1_minivec/target/debug/deps/week1_minivec-92b9146ff3acad09.d: src/main.rs
+
+/workspaces/Rust_primer/week1_minivec/target/debug/deps/libweek1_minivec-92b9146ff3acad09.rmeta: src/main.rs
+
+src/main.rs:
